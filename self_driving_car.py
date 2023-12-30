72b828ffe138884919d22415d631dd41e4ecd844
ndavido <ndavido02@gmail.com>
2023-12-30 15:08:00 +0000

    :memo: Uploaded Starter Code

--- self_driving_car.py
+++ self_driving_car.py
@@ -1,1 +1,157 @@
 #! usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+import keras
+from keras.models import Sequential
+from keras.optimizers import Adam
+from keras.layers import Dense, Convolution2D, MaxPooling2D, Dropout, Flatten
+import cv2
+import pandas as pd
+import random
+import os
+import ntpath
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+import matplotlib.image as mpimg
+
+datadir = "DrivingData"
+columns = ['center', 'left', 'right',
+           'steering', 'throttle', 'reverse', 'speed']
+data = pd.read_csv(os.path.join(datadir, 'driving_log.csv'), names=columns)
+print(data.head)
+
+
+def path_leaf(path):
+    head, tail = ntpath.split(path)
+    return tail
+
+
+data['center'] = data['center'].apply(path_leaf)
+data['left'] = data['left'].apply(path_leaf)
+data['right'] = data['right'].apply(path_leaf)
+
+print(data.head)
+
+num_bins = 25
+hist, bins = np.histogram(data['steering'], num_bins)
+print(bins)
+
+centre = (bins[:-1] + bins[1:])*0.5
+samples_per_bin = 250
+plt.bar(centre, hist, width=0.05)
+plt.plot((np.min(data['steering']), np.max(
+    data['steering'])), (samples_per_bin, samples_per_bin))
+
+remove_list = []
+for j in range(num_bins):
+    list_ = []
+    for i in range(len(data['steering'])):
+        if bins[j] <= data['steering'][i] <= bins[j+1]:
+            list_.append(i)
+    list_ = shuffle(list_)
+    list_ = list_[samples_per_bin:]
+    remove_list.extend(list_)
+
+
+print("Removed: ", len(remove_list))
+data.drop(data.index[remove_list], inplace=True)
+print("Remaining: ", len(data))
+
+hist, bins = np.histogram(data['steering'], num_bins)
+plt.bar(centre, hist, width=0.05)
+plt.plot((np.min(data['steering']), np.max(
+    data['steering'])), (samples_per_bin, samples_per_bin))
+
+
+def load_img_steering(datadir, df):
+    image_path = []
+    steering = []
+    for i in range(len(data)):
+        indexed_data = data.iloc[i]
+        center, left, right = indexed_data[0], indexed_data[1], indexed_data[2]
+        image_path.append(os.path.join(datadir, center.strip()))
+        steering.append(float(indexed_data[3]))
+    image_paths = np.asarray(image_path)
+    steerings = np.asarray(steering)
+    return image_paths, steerings
+
+
+image_paths, steerings = load_img_steering(datadir+'/IMG', data)
+X_train, X_valid, y_train, y_valid = train_test_split(
+    image_paths, steerings, test_size=0.2, random_state=6)
+
+print(f"Traing samples {len(X_train)}, validation samples {len(X_valid)}")
+
+fig, axes = plt.subplots(1, 2, figsize=(12, 4))
+axes[0].hist(y_train, bins=num_bins, width=0.05, color='blue')
+axes[0].set_title('Training set')
+axes[1].hist(y_valid, bins=num_bins, width=0.05, color='red')
+axes[1].set_title('Validation set')
+
+
+def img_preprocess(img):
+    img = mpimg.imread(img)
+    img = img[60:135, :, :]
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+    img = cv2.GaussianBlur(img, (3, 3), 0)
+    img = cv2.resize(img, (200, 66))
+    img = img/255
+    return img
+
+
+image = image_paths[100]
+original_image = mpimg.imread(image)
+preprocessed_image = img_preprocess(image)
+fig, axes = plt.subplots(1, 2, figsize=(15, 10))
+fig.tight_layout()
+axes[0].imshow(original_image)
+axes[0].set_title("Original Image")
+axes[1].imshow(preprocessed_image)
+axes[1].set_title("Preprocessed Image")
+
+X_train = np.array(list(map(img_preprocess, X_train)))
+X_valid = np.array(list(map(img_preprocess, X_valid)))
+
+plt.imshow(X_train[random.randint(0, len(X_train)-1)])
+
+# https://arxiv.org/pdf/1604.07316v1.pdf
+
+
+def nvidia_model():
+    model = Sequential()
+    model.add(Convolution2D(24, kernel_size=(5, 5), strides=(
+        2, 2), input_shape=(66, 200, 3), activation='elu'))
+    model.add(Convolution2D(36, kernel_size=(5, 5),
+              strides=(2, 2), activation='elu'))
+    model.add(Convolution2D(48, kernel_size=(5, 5),
+              strides=(2, 2), activation='elu'))
+    model.add(Convolution2D(64, kernel_size=(3, 3), activation='elu'))
+    model.add(Convolution2D(64, kernel_size=(3, 3), activation='elu'))
+    model.add(Dropout(0.5))
+    model.add(Flatten())
+    model.add(Dense(100, activation='elu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(50, activation='elu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(10, activation='elu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1))
+
+    optimizer = Adam(learning_rate=0.001)
+    model.compile(loss='mse', optimizer=optimizer)
+    return model
+
+
+model = nvidia_model()
+print(model.summary())
+
+history = model.fit(X_train, y_train, epochs=30, validation_data=(
+    X_valid, y_valid), batch_size=100, verbose=1, shuffle=1)
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.legend(['Training', 'Validation'])
+plt.title('Loss')
+plt.xlabel('Epoch')
+
+model.save('alpha_model.h5')
